05d4d3ed555e167d76c493f2097db60656bb4a57
Van Garman <armand.giovani21@gmail.com>
2023-11-27 08:59:48 +0700

    feat: add mvc crud

--- Kuliah 12/CRUD/application/views/template/footer.php
+++ Kuliah 12/CRUD/application/views/template/footer.php
@@ -1,7 +1,7 @@
 	<footer class="footer">
 	  <div class="container" style="text-align:center;">
 	    <hr />
-	    <span>Fathin Muhashibi Putra - 5025211229 - PBKK A <?php echo date('Y'); ?></span>
+	    <span>M.Armand Giovani - 5025211054 - PBKK A <?php echo date('Y'); ?></span>
 	  </div>
 	</footer>
 	</body>
---
+++ Kuliah 13/README.md
@@ -0,0 +1,12 @@
+| Nama              | NRP        |
+| ----------------- | ---------- |
+| M. Armand Giovani | 5025211054 |
+
+## Deskripsi Aplikasi
+
+Membuat MVC CRUD Toko Online Menggunakan CodeIgniter 3.
+
+## Dokumentasi
+
+## Link Blog : https://vangarman21.blogspot.com/2023/11/kuliah-12-pbkk.html
+
